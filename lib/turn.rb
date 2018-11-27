ef764bc345f0b640901d25b33dbdcdc294c505bc
earl-stephens <stephensearl@hotmail.com>
2018-11-26 18:14:32 -0700

    added methods to turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,5 +7,26 @@ class Turn
     @card = card
   end
 
+  def card
+    @card
+  end
+
+  def guess
+    @guess
+  end
+
+  def correct?
+    @guess == card.answer
+  end
+
+  # print a feedback statement
+  def feedback
+    if @guess == card.answer
+      puts "Correct!"
+    else
+      puts "Incorrect."
+    end
+  end
+
 
 end
