590005a2a90aa2201fdbf8b99669b4973d99430f
earl-stephens <stephensearl@hotmail.com>
2018-11-28 16:51:18 -0700

    added test to check for a deck

--- test/round_test.rb
+++ test/round_test.rb
@@ -8,7 +8,7 @@ require 'pry'
 
 class RoundTest < Minitest::Test
 
-def test_the_first_deck
+def test_it_exists
   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -17,5 +17,13 @@ def test_the_first_deck
   assert_instance_of Round, round
 end
 
+def test_check_for_deck
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  assert_instance_of Deck ,round.deck
+end
 
 end
