10e6785167569543fd2f3c48b2da54c461fc33df
earl-stephens <stephensearl@hotmail.com>
2018-11-28 08:28:44 -0700

    added test for counting categories

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,9 +1,25 @@
 class Deck
+  attr_reader(:cards, :last)
 
-attr_accessor(:cards)
+  def initialize(cards)
+    @cards = cards
+    @last = []
+  end
 
-  def initialize
-    @cards = []
+  def number_of_cards
+    @cards.length
+  end
+
+  def find_last_elements
+    @last = []
+    @last << cards[0].category
+    @last << cards[1].category
+    @last << cards[2].category
+  end
+
+  def cards_in_a_particular_category
+@last.count{|x| x == :STEM}
+
   end
 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -2,10 +2,68 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/turn'
 require './lib/card'
-require './lib.deck'
+require './lib/deck'
+require 'pry'
 
 class DeckTest < Minitest::Test
 
+  def test_the_first_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    assert_instance_of Card, card
+  end
+
+  def test_the_second_card
+    card = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    assert_instance_of Card, card
+  end
+
+  def test_it_exists
+    card = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    assert_instance_of Card, card
+  end
+
+  def test_that_there_is_a_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
+  end
+
+  def test_the_number_of_cards_in_the_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal 3, deck.number_of_cards
+  end
+
+def test_find_last_elements
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  cards = [card_1, card_2, card_3]
+  deck = Deck.new(cards)
+  assert_equal [:Geography, :STEM, :STEM], deck.find_last_elements
+end
+
+  def test_cards_in_a_particular_category
+# skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    deck.find_last_elements
+    binding.pry
+    assert_equal 2, deck.cards_in_a_particular_category
+  end
 
 
 end
