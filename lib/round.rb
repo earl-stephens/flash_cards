f99ae42ea844ac3f1dbcf095bfcb1b9ac200bd5f
earl-stephens <stephensearl@hotmail.com>
2018-11-29 15:10:08 -0700

    added tests to round_test and round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,16 +1,32 @@
 class Round
-attr_reader(:deck)
+  require './lib/turn'
+attr_reader :deck,
+            :current_card,
+            :new_turn,
+            :turns,
+            :num_right
 
 def initialize(deck_arg)
   @deck = deck_arg
-end
-
-def turns
-  turns = []
+  @turns = []
+  @num_right = 0
+  # @current_card = card_1
 end
 
 def current_card
   deck.get_a_card(0)
+end
+
+def take_turn(my_guess)
+  card_variable = self.current_card
+  @new_turn = Turn.new(my_guess, card_variable)
+  @turns.push(@new_turn)
+end
+
+def number_correct
+  if @new_turn.correct?
+    @num_right += 1
+  end
 end
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -46,4 +46,71 @@ def test_for_current_card
   assert_equal card_1, round.current_card
 end
 
+def test_for_take_turn_method
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  round.take_turn("Juneau")
+  assert_instance_of Turn, round.new_turn
+end
+
+def test_for_new_turn_class
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  round.take_turn("Juneau")
+  assert_equal Turn, round.new_turn.class
+end
+
+def test_for_correct_guess
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  round.take_turn("Juneau")
+  assert round.new_turn.correct?
+end
+
+def test_for_turns_being_updated
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  round.take_turn("Juneau")
+  # binding.pry
+  assert_equal round.turns, round.take_turn("Juneau")
+end
+
+def test_for_number_of_correct_guesses
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  round.take_turn("Juneau")
+  assert_equal 1, round.number_correct
+end
+
+def test_for_next_current_card
+  skip
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  round.take_turn("Juneau")
+  assert_equal
+end
+# The take_turn method is the crux of this problem.
+# The take_turn method takes a string representing the guess.
+# It should create a new Turn object with the appropriate guess
+# and Card. It should store this new Turn, as well as return
+# it from the take_turn method. Also, when the take_turn method
+# is called, the Round should move on to the next card in the deck.
 end
