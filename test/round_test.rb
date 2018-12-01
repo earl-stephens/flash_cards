cfa6939505ef0388a3363f9a04f007b9b5bc1019
earl-stephens <stephensearl@hotmail.com>
2018-12-01 14:11:35 -0700

    last test for iteration 2

--- test/round_test.rb
+++ test/round_test.rb
@@ -88,14 +88,16 @@ def test_for_turns_being_updated
 end
 
 def test_for_number_of_correct_guesses
-  skip
+  # skip
   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
   deck = Deck.new([card_1, card_2, card_3])
   round = Round.new(deck)
+  # binding.pry
   round.take_turn("Juneau")
-  assert_equal 1, round.number_correct
+  # binding.pry
+  assert_equal 1, round.num_right
 end
 
 def test_for_next_current_card
@@ -153,7 +155,7 @@ def test_for_number_of_correct_guesses_after_giving_a_wrong_answer
 end
 
 def test_for_number_of_correct_guesses_by_category
-  skip
+  # skip
   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
