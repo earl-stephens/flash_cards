81e4c5985861390f08a35ebc8e8afa4209c0aa7d
earl-stephens <stephensearl@hotmail.com>
2018-11-30 22:22:17 -0700

    added test for updating turn array after take_turn

--- test/round_test.rb
+++ test/round_test.rb
@@ -118,4 +118,15 @@ def test_for_incorrect_guess_on_second_card
   refute round.take_turn("Venus").correct?
 end
 
+def test_for_turns_array_being_updated_after_take_turn
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  round.take_turn("Juneau")
+  round.take_turn("Venus")
+  assert_equal 2, round.turns.length
+end
+
 end
