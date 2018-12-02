f903d574d48d3029979b8454b4463e2998e38152
earl-stephens <stephensearl@hotmail.com>
2018-12-02 12:38:26 -0700

    added more to the cardgenerator cards method

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,14 +1,27 @@
+require './lib/card'
+require 'pry'
+
 class CardGenerator
 attr_reader :filename
+            :cardname
 
   def initialize(filename_arg)
     @filename = filename
+    @cardname
   end
 
 def cards
 IO.foreach("./lib/cards.txt") do |line|
-  puts line
+  counter = 1
+  clean_line = line.strip.split(',')
+  question = clean_line[0]
+  answer = clean_line[1]
+  category = clean_line[2]
+   @cardname = "card_#{counter}"
+   binding.pry
+   @cardname = Card.new(question, answer, category)
+   counter += 1
 end
+return @cardname
 end
-
 end
--- test/cardgenerator_test.rb
+++ test/cardgenerator_test.rb
@@ -8,9 +8,10 @@ class CardGeneratorTest < Minitest::Test
 
   def test_it_exists
     cards = CardGenerator.new("cards.txt").cards
-    assert_instance_of CardGenerator, cards
+    assert_instance_of Card, cards
   end
 
-
+def test_for_cards
+end
 
 end
