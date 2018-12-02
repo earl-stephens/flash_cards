acb0c10ca0020ca2e7c23fb57d5d57b198d4a9bc
earl-stephens <stephensearl@hotmail.com>
2018-12-01 21:30:54 -0700

    finished off flashcard_runner file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -12,8 +12,8 @@ round = Round.new(deck)
 
 #initial output to user
 puts "Welcome!  You're playing with " + deck.count.to_s + " cards."
-puts "__________________________________________________"
-binding.pry
+puts "------------------------------------------------------------"
+# binding.pry
 counter_variable = round.current_card_number + 1
 while round.turns.count < deck.cards.length
 puts "This is card " + counter_variable.to_s + " out of " + deck.count.to_s
@@ -29,6 +29,7 @@ counter_variable += 1
 
 end
 
-#move to next card
-# puts "This is card " + round.current_card_number.to_s + " out of " + deck.count.to_s
-# puts "Question: " + round.current_card.question
+puts "****** Game over! ******"
+puts "You had " + round.num_right.to_s + " correct guesses out of " +
+      round.turns.count.to_s + " for a total score of " +
+      round.percent_correct.to_i.to_s + "%."
